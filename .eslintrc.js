1ca41108f6821a3add0a478099f4cb4b4db55364
Matheus Iacono <matheus@iacono.com.br>
2018-03-20 13:04:26 -0300

    adding eslint and prettier-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: 'airbnb',
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to'],
+      },
+    ],
+  },
+  settings: {
+    'import/core-modules': ['react', 'prop-types'],
+  },
+  globals: {
+    graphql: false,
+  },
+};
