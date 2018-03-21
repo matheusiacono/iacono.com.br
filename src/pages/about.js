3d70adf79c6f0dcb4f3b43674f7afb88aa50308a
Matheus Iacono <matheus@iacono.com.br>
2018-03-21 03:30:52 -0300

    creating about page

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+export const data = {
+  title: 'Choropleth on d3v4',
+  written: '2017-05-04',
+  layoutType: 'post',
+  path: 'choropleth-on-d3v4',
+  category: 'data science',
+  description: 'Things about the choropleth.',
+};
+
+const AboutPage = () => (
+  <Fragment>Sobre</Fragment>
+);
+
+
+export default AboutPage;
