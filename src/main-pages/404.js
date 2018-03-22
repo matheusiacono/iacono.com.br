720b79a80858d5c2759a362a1f85771a063c19e3
Matheus Iacono <matheus@iacono.com.br>
2018-03-22 11:30:47 -0300

    adding title to error page

--- src/main-pages/404.js
+++ src/main-pages/404.js
@@ -3,7 +3,9 @@ import React from 'react';
 import Main from '../containers/main';
 
 const ErrorPage = () => (
-  <Main>Página não encontrada</Main>
+  <Main title="Página não encontrada">
+    Página não encontrada
+  </Main>
 );
 
 export default ErrorPage;
