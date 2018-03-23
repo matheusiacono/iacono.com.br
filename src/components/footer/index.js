7f252567eab994e47eb92a21c688945f7385e8df
Matheus Iacono <matheus@iacono.com.br>
2018-03-22 22:13:52 -0300

    styling footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,6 +1,48 @@
 import React from 'react';
 import g from 'glamorous';
 
+const SweepToRight = g.a({
+  background: '#999',
+  color: '#333',
+
+  display: 'flex',
+  verticalAlign: 'middle',
+  transform: 'perspective(1px) translateZ(0)',
+  boxShadow: '0 0 1px transparent',
+  transitionProperty: 'color',
+  transitionDuration: '0.3s',
+
+  ':before': {
+    content: '""',
+    position: 'absolute',
+    zIndex: '-1',
+    top: '0',
+    left: '0',
+    right: '0',
+    bottom: '0',
+    background: '#333',
+    transform: 'scaleX(0)',
+    transformOrigin: '0 50%',
+    transitionProperty: 'transform',
+    transitionDuration: '0.3s',
+    transitionTimingFunction: 'ease-out',
+  },
+
+  ':hover,:focus,:active': {
+    color: 'white',
+  },
+
+  ':hover:before,:focus:before,:active:before': {
+    transform: 'scaleX(1)',
+  },
+});
+
+const ExternalLink = g(SweepToRight)({
+  display: 'block',
+  padding: '0.5rem 1rem',
+  textDecoration: 'none',
+});
+
 const Footer = () => {
   const items = [
     { text: 'Github', path: '//github.com/matheusiacono' },
@@ -15,13 +57,18 @@ const Footer = () => {
       flexShrink="0"
     >
       <p>&copy; 2018 Matheus Iácono</p>
-      <g.Ul listStyle="none" display="flex" marginLeft="auto">
+      <g.Ul listStyle="none" display="flex" marginLeft="auto" padding="0">
         {items.map(item => (
-          <li>
-            <a target="_blank" rel="noopener noreferrer" href={item.path} title={item.text}>
+          <g.Li flex="1 1 auto" marginRight=".2rem" textDecoration="none" key={item.text}>
+            <ExternalLink
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.path}
+              title={item.text}
+            >
               {item.text}
-            </a>
-          </li>
+            </ExternalLink>
+          </g.Li>
         ))}
       </g.Ul>
     </g.Footer>
