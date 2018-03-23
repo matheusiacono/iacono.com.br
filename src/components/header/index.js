847beec66f359b353e19591b44f0fee699ff64d4
Matheus Iacono <matheus@iacono.com.br>
2018-03-23 14:46:52 -0300

    creating header link

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import g, { Hr, H1 } from 'glamorous';
+import Link from 'gatsby-link';
+import g from 'glamorous';
+
+const RootLink = g(Link)({
+  color: 'inherit',
+  textDecoration: 'none',
+});
 
 const Header = ({ menu }) => {
   const Menu = menu;
   Menu.displayName = menu.displayName;
   return (
     <g.Header flexShrink="0">
-      <H1 display="flex" justifyContent="center" margin="0" padding="1rem 0">Matheus Iácono | Desenvolvedor Front End</H1>
-      <Hr margin="0" />
+      <g.H1 display="flex" fontSize="2.5em" justifyContent="center" margin="0" padding="1rem 0">
+        <RootLink to="/">Matheus Iácono - Desenvolvedor Front End</RootLink>
+      </g.H1>
       <Menu />
     </g.Header>
   );
