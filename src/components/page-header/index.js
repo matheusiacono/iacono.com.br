2c75aebe10d5ea3fdec1db5048bc5cd77b251067
Matheus Iacono <matheus@iacono.com.br>
2018-03-26 03:31:30 -0300

    handling header on window scroll event

--- src/components/page-header/index.js
+++ src/components/page-header/index.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import { presets } from 'glamor';
+/* globals window */
+import React, { Component } from 'react';
+import { keyframes, presets } from 'glamor';
 import g from 'glamorous';
 
 import Logo from '../logo';
 import SocialLinks from '../social-links';
 import Menu from '../menu';
 
-const Header = g.header({
-  position: 'absolute',
+import colors from '../../styles/colors';
+
+const slideDownAnimation = keyframes({
+  '0%': {
+    transform: 'translateY(-100%)',
+  },
+  '100%': {
+    transform: 'translateY(0)',
+  },
+});
+
+const slideUpAnimation = keyframes({
+  '0%': {
+    transform: 'translateY(0)',
+  },
+  '100%': {
+    transform: 'translateY(-100%)',
+  },
+});
+
+const Header = g.header(({ slideUp, onTop, notScrolled }) => ({
+  position: 'fixed',
+  backgroundColor: onTop ? 'transparent' : colors.dark(),
   color: '#FFF',
   width: '100%',
   top: '0',
   zIndex: '4',
   paddingTop: '1rem',
+  transition: 'background-color .3s ease',
+  animationDuration: '.3s',
+  animationFillMode: 'both',
+  animationName: !notScrolled && (slideUp ? slideDownAnimation : slideUpAnimation),
 
   display: 'grid',
   gridTemplateColumns: '2rem 128px auto 128px 2rem',
@@ -23,7 +49,7 @@ const Header = g.header({
     alignItems: 'center',
     gridTemplateRows: '100px',
   },
-});
+}));
 
 const LogoWrapper = g.div({
   gridColumn: '2',
@@ -45,18 +71,56 @@ const SocialLinksWrapper = g.div({
   gridRow: '1',
 });
 
-const PageHeader = () => (
-  <Header>
-    <LogoWrapper>
-      <Logo />
-    </LogoWrapper>
-    <Nav>
-      <Menu />
-    </Nav>
-    <SocialLinksWrapper>
-      <SocialLinks />
-    </SocialLinksWrapper>
-  </Header>
-);
+class PageHeader extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      scrollY: 0,
+      slideUp: false,
+      onTop: true,
+      notScrolled: true,
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    this.setState(prevState => ({
+      scrollY: window.scrollY,
+      slideUp: window.scrollY <= this.state.scrollY,
+      onTop: window.scrollY === 0,
+      notScrolled: prevState.scrollY === 0 && window.scrollY !== 0,
+    }));
+  }
+
+  render() {
+    return (
+      <Header
+        slideUp={this.state.slideUp}
+        notScrolled={this.state.notScrolled}
+        onTop={this.state.onTop}
+      >
+        <LogoWrapper>
+          <Logo />
+        </LogoWrapper>
+        <Nav>
+          <Menu />
+        </Nav>
+        <SocialLinksWrapper>
+          <SocialLinks />
+        </SocialLinksWrapper>
+      </Header>
+    );
+  }
+}
 
 export default PageHeader;
