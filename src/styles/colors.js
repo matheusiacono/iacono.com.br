6aeba174afa25739cc9425343d02b196c977e0f9
Matheus Iacono <matheus@iacono.com.br>
2018-03-26 01:58:44 -0300

    adjusting colors file

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,13 +1,6 @@
 export default {
-  text: '#23395B',
   dark: alpha => (alpha ? `rgba(35, 57, 91, ${alpha})` : '#23395B'),
-  link: '#406E8E',
-  primary: '#83C9F4',
-  secondary: '#CBF7ED',
-  terciary: '#23395B',
-
-  externalPrimary: '#0F8B8D',
-  externalSecundary: '#05668D',
-
-  info: '#550C18',
+  light: () => '#CBF7ED',
+  primary: () => '#83C9F4',
+  secondary: () => '#406E8E',
 };
