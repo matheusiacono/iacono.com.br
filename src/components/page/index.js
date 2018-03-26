541f8d5b494559da28e9332fc7066f6a7560a395
Matheus Iacono <matheus@iacono.com.br>
2018-03-26 01:49:43 -0300

    creating Page component

---
+++ src/components/page/index.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import 'glamor/reset';
+
+import PageHeader from '../page-header';
+import Post from '../post';
+
+import appendTitle from '../../utils/append-title';
+import '../../styles/fonts';
+
+const Page = props => (
+  <Fragment>
+    <Helmet title={appendTitle(props.pageTitle, 'Matheus Iácono')} />
+    <PageHeader />
+    <Post {...props} />
+  </Fragment>
+);
+
+Page.propTypes = {
+  pageTitle: PropTypes.string,
+};
+
+Page.defaultProps = {
+  pageTitle: '',
+};
+
+export default Page;
