2a7889c0e994cbe6aa375b718e504e1d9de18ea6
Matheus Iacono <matheus@iacono.com.br>
2018-03-26 01:44:54 -0300

    creating PostContent component

---
+++ src/components/post-content/index.js
@@ -0,0 +1,10 @@
+import g from 'glamorous';
+
+const PostContent = g.div({
+  position: 'relative',
+  backgroundColor: '#FFF',
+  minHeight: '100vh',
+  zIndex: '1',
+});
+
+export default PostContent;
