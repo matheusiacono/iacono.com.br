7a331be0e5a82a1d54c958d907069a624dd2854c
Matheus Iacono <matheus@iacono.com.br>
2018-03-27 21:32:35 -0300

    adding subtitle to header and moving font-family and color to it

--- src/components/post-header-title/index.js
+++ src/components/post-header-title/index.js
@@ -3,12 +3,9 @@ import g from 'glamorous';
 
 const PostHeaderTitle = g.h1({
   margin: '.3em 0',
-  fontFamily: 'Montserrat, sans-serif',
   fontSize: '3em',
-  color: '#FFF',
   textAlign: 'center',
   zIndex: '3',
-  marginTop: '4em',
   [presets.Tablet]: {
     fontSize: '4em',
   },
--- src/components/post-header/index.js
+++ src/components/post-header/index.js
@@ -3,31 +3,40 @@ import PropTypes from 'prop-types';
 import g from 'glamorous';
 
 import PostHeaderTitle from '../post-header-title';
+import PostHeaderSubtitle from '../post-header-subtitle';
 import PostHeaderImage from '../post-header-image';
 import colors from '../../styles/colors';
 
 const Header = g.header({
+  fontFamily: 'Montserrat, sans-serif',
+  color: '#FFF',
   backgroundColor: colors.dark(),
   minHeight: '600px',
   height: '100vh',
   width: '100%',
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'center',
+  justifyContent: 'flex-end',
   position: 'relative',
   flexDirection: 'column',
 });
 
-const PostHeader = ({ title, image }) => (
+const PostHeader = ({ title, subtitle, image }) => (
   <Header>
     <PostHeaderTitle>{title}</PostHeaderTitle>
+    <PostHeaderSubtitle>{subtitle}</PostHeaderSubtitle>
     <PostHeaderImage image={image} />
   </Header>
 );
 
 PostHeader.propTypes = {
   title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string,
   image: PropTypes.string.isRequired,
+};
+
+PostHeader.defaultProps = {
+  subtitle: ' ',
 };
 
 export default PostHeader;
