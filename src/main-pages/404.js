babd97d027a58adca5dd384292bec43438c0a37f
Matheus Iacono <matheus@iacono.com.br>
2018-03-30 14:58:11 -0300

    changing 404 image

--- src/main-pages/404.js
+++ src/main-pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Page from '../components/page';
 
-import postImage from '../assets/img/home-background.jpeg';
+import postImage from '../assets/img/404-background.jpeg';
 
 const ErrorPage = () => (
   <Page pageTitle="Página não encontrada" title="Página não encontrada" image={postImage} />
