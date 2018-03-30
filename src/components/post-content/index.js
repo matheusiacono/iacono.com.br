dfd0fd2b142e099e94384b8f03a9c43f68e48343
Matheus Iacono <matheus@iacono.com.br>
2018-03-30 14:58:47 -0300

    styling Post Content component

--- src/components/post-content/index.js
+++ src/components/post-content/index.js
@@ -1,10 +1,36 @@
+import React from 'react';
 import g from 'glamorous';
 
-const PostContent = g.div({
+import colors from '../../styles/colors';
+import ChildrenType from '../../utils/children-type';
+
+const Content = g.div({
   position: 'relative',
   backgroundColor: '#FFF',
   minHeight: '100vh',
   zIndex: '1',
+  padding: '90px 0',
+
+  lineHeight: '1.8em',
+  fontSize: '1.2em',
+  color: colors.text(),
+  fontFamily: 'Lora',
 });
+
+const Container = g.div({
+  margin: '0 auto',
+  maxWidth: '700px',
+  padding: '0 20px',
+});
+
+const PostContent = ({ children }) => (
+  <Content>
+    <Container>{children}</Container>
+  </Content>
+);
+
+PostContent.propTypes = {
+  children: ChildrenType.isRequired,
+};
 
 export default PostContent;
