bc8b1b11ab379fa7a13fec20d6766e9ae165e4e3
Matheus Iacono <matheus@iacono.com.br>
2018-03-30 21:01:47 -0300

    adding text to index page

--- src/main-pages/index.js
+++ src/main-pages/index.js
@@ -6,7 +6,11 @@ import postImage from '../assets/img/home-background.jpeg';
 
 const IndexPage = () => (
   <Page title="Sempre Aprendendo" image={postImage}>
-    {' '}
+    <p>
+      Olá, me chamo Matheus Iácono, sou desenvolvedor front end e moro em São Paulo. Antes de mudar
+      para cá, morava em Aracaju onde passei a maior parte da minha vida. Gosto de estar
+      constantemente estudando e de compartilhar o conhecimento com outras pessoas.
+    </p>
   </Page>
 );
 
