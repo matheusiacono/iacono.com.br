3ee28ce4adccdd2095fb170e730832cf0d81508b
Matheus Iacono <matheus@iacono.com.br>
2018-03-31 16:34:32 -0300

    adding alpha to secondary color

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -2,6 +2,6 @@ export default {
   dark: alpha => (alpha ? `rgba(35, 57, 91, ${alpha})` : '#23395B'),
   light: () => '#CBF7ED',
   primary: () => '#83C9F4',
-  secondary: () => '#406E8E',
+  secondary: alpha => (alpha ? `rgba(64,110,142, ${alpha})` : '#406E8E'),
   text: () => '#333',
 };
