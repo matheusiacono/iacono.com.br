01116c796927544825c4a536d65963b011335636
Matheus Iacono <matheus@iacono.com.br>
2018-03-31 18:37:48 -0300

    adding new background to contact page

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Page from '../components/page';
 
-import postImage from '../assets/img/home-background.jpeg';
+import postImage from '../assets/img/contato-background.jpeg';
 
 const ContactPage = () => <Page pageTitle="Contato" title="Contato" image={postImage} />;
 
